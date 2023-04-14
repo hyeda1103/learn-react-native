de0da3a40a7244359f2b2eb5defedfba07ec67f4
hyeda1103 <dalgona92@gmail.com>
2023-04-14 20:36:44 +0900

    feat: draw basic todolist on screen

--- App.tsx
+++ App.tsx
@@ -5,27 +5,33 @@
  * @format
  */
 
-import React from 'react';
-import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context"
+import React, {useState} from 'react';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 import DateHead from './components/DateHead';
 import AddTodo from './components/AddTodo';
 import Empty from './components/Empty';
-import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
+import {StyleSheet, KeyboardAvoidingView, Platform} from 'react-native';
+import TodoList from './components/TodoList';
 
 export default function App() {
-  const today = new Date()
+  const today = new Date();
+
+  const [todos, setTodos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
 
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={["bottom"]} style={styles.block}>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
         <KeyboardAvoidingView
           behavior={Platform.select({ios: 'padding'})}
-          style={styles.avoid}
-        >
-        <DateHead date={today} />
-        <Empty />
-        <AddTodo />
+          style={styles.avoid}>
+          <DateHead date={today} />
+          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
+          <AddTodo />
         </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
@@ -35,9 +41,9 @@ export default function App() {
 const styles = StyleSheet.create({
   block: {
     flex: 1,
-    backgroundColor: "#fff"
+    backgroundColor: '#fff',
   },
   avoid: {
-    flex: 1
-  }
-})
+    flex: 1,
+  },
+});
---
+++ components/TodoList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {FlatList, View, Text, StyleSheet} from 'react-native';
+import TodoItem from './TodoItem';
+
+export default function TodoList({todos}) {
+  return (
+    <FlatList
+      style={styles.list}
+      data={todos}
+      renderItem={({item}) => (
+        <TodoItem id={item.id} text={item.text} done={item.done} />
+      )}
+      keyExtractor={item => item.id.toString()}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+  },
+});
